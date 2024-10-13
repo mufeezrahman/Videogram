7609a099e68552f92532aa02d2902e365a79b931
mufeezrahman <mufeezrahman15@gmail.com>
2024-10-13 23:36:34 +0530

    Added comment,playlist, tweet and likes models

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -26,6 +26,7 @@ userRouter.route("/change-password").post(verifyJWT,changePassword)
 userRouter.route("/current-user").get(verifyJWT,getCurrentUser)
 userRouter.route("/update-details").patch(verifyJWT,updateAccountDetails)
 userRouter.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+userRouter.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserAvatar)
 userRouter.route("/c/:username").get(verifyJWT,getUserChannelProfile)
 userRouter.route("/history").get(verifyJWT,getWatchHistory)
 export default userRouter;
